c51e2cf52b88af53dd5ab7879b68c9a2aad3428a
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-03-07 21:58:04 +0100

    fixing filter 2

--- src/main/java/com/example/tic_tac_toe_backend/filter/CORSFilter.java
+++ src/main/java/com/example/tic_tac_toe_backend/filter/CORSFilter.java
@@ -15,23 +15,20 @@ import java.io.IOException;
 public class CORSFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         if (servletResponse instanceof HttpServletResponse response) {
             HttpServletRequest request = (HttpServletRequest) servletRequest;
             response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
             response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS, HEAD");
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Headers", "authorization,access-control-allow-credentials,access-control-allow-headers,access-control-allow-origin,content-type");
             if ("OPTIONS".equalsIgnoreCase((request.getMethod()))) {
                 response.setStatus(HttpServletResponse.SC_OK);
             } else {
-                filterChain.doFilter(request, response);
+                chain.doFilter(request, response);
             }
             log.info("Added CORS headers to response using CORSFilter");
-
         }
-
     }
 }
