3089ff666c60abd21515de3872353e044f200233
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-03-12 19:19:36 +0100

    refactor

--- src/main/java/com/example/tic_tac_toe_backend/config/RoomController.java
+++ src/main/java/com/example/tic_tac_toe_backend/config/RoomController.java
@@ -4,6 +4,7 @@ import com.example.tic_tac_toe_backend.dto.RoomDTO;
 import com.example.tic_tac_toe_backend.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class RoomController {
 
     private final RoomService boardService;
 
-    @RequestMapping("/chooseRoomForPlayer")
+    @PostMapping("/chooseRoomForPlayer")
     public ResponseEntity<RoomDTO> chooseRoomForPlayer(@RequestParam String playerName) {
         return ResponseEntity.ok(boardService.chooseRoomForPlayer(playerName));
     }
