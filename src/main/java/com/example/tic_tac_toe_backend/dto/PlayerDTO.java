45ae4db790a700193c6ab2e151980c98c4f0d63c
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-03-14 20:53:22 +0100

    update
    add dtype to player

--- src/main/java/com/example/tic_tac_toe_backend/dto/PlayerDTO.java
+++ src/main/java/com/example/tic_tac_toe_backend/dto/PlayerDTO.java
@@ -10,11 +10,13 @@ import lombok.Setter;
 @Builder
 public class PlayerDTO {
 
+    private String dtype = "Player";
     private String name;
     private boolean isStarting;
 
     public static PlayerDTO of(Player player) {
         return PlayerDTO.builder()
+                .dtype("Player")
                 .name(player.getName())
                 .isStarting(player.isStarting())
                 .build();
