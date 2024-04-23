8f8aa0bf1e1cc10c2b15440b1dc1390af9841dd0
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-04-23 12:19:12 +0200

    add security and jwt auth

---
+++ src/main/java/com/example/tic_tac_toe_backend/utils/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.tic_tac_toe_backend.utils.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return httpSecurity.build();
+
+    }
+
+}
