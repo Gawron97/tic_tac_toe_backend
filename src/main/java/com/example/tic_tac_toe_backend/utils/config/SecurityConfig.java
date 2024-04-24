954c70335532c714c2297bad9071060744261e09
Gawron97 <89856910+Gawron97@users.noreply.github.com>
2024-04-24 14:50:08 +0200

    cleanup code

--- src/main/java/com/example/tic_tac_toe_backend/utils/config/SecurityConfig.java
+++ src/main/java/com/example/tic_tac_toe_backend/utils/config/SecurityConfig.java
@@ -1,46 +1,22 @@
 package com.example.tic_tac_toe_backend.utils.config;
 
-import com.example.tic_tac_toe_backend.utils.filter.AddParameterToHeaderFilter;
-import com.example.tic_tac_toe_backend.utils.filter.CORSFilter;
 import com.example.tic_tac_toe_backend.utils.filter.CustomTokenResolver;
-import com.example.tic_tac_toe_backend.utils.filter.WebsocketConnectionFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationManagerResolver;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
-import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.Arrays;
-import java.util.List;
 
 @EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 @Slf4j
 @Configuration
 public class SecurityConfig {
-
-    private final WebsocketConnectionFilter websocketConnectionFilter;
-    private final ApplicationContext applicationContext;
-    private final AddParameterToHeaderFilter addParameterToHeaderFilter;
 
 
     @Bean
--- src/main/java/com/example/tic_tac_toe_backend/utils/config/WebsocketConfiguration.java
+++ src/main/java/com/example/tic_tac_toe_backend/utils/config/WebsocketConfiguration.java
@@ -1,11 +1,7 @@
 package com.example.tic_tac_toe_backend.utils.config;
 
-import com.example.tic_tac_toe_backend.utils.filter.CustomHandshakeHandler;
-import com.example.tic_tac_toe_backend.utils.filter.HttpHandshakeInterceptor;
-import com.example.tic_tac_toe_backend.utils.filter.JwtChannelInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -23,19 +19,12 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                .addInterceptors(new HttpHandshakeInterceptor())
-                .setHandshakeHandler(new CustomHandshakeHandler(jwtDecoder))
                 .withSockJS(); // endpoint for client to connect to the server
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic"); // prefix for server to send messages to the client
         registry.setApplicationDestinationPrefixes("/app"); // prefix for client to send messages to the server
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new JwtChannelInterceptor());
     }
 
 }
